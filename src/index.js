0925cc32238fad8daefbeae079a759bb58a274e2
Davide Morri <davide.morri@mondora.com>
2017-02-17 16:02:32 +0900

    [fix] take right params and submit right value format

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import "babel-polyfill";
+import moment from "moment";
 
 exports.handler = function(event, context) {
     console.log("Received event:", JSON.stringify(event, null, 2));
@@ -8,25 +9,23 @@ exports.handler = function(event, context) {
     const {zuoraApiHost} = event["stage-variables"];
     const url = `https://${zuoraApiHost}/rest/v1/accounts`;
 
-    //TODO
-    const billing = {};
-    const paymentMethodId = "";
-    const planId = "";
+    const {billingInfo, subscriptionPlanId, paymentMethodId} = event["body-json"];
 
+    //TODO verify all parameters
     axios.post(url, {
         name: "TODO",
         currency: "EUR",
         billToContact: {
-            firstName: billing.givenName,
-            lastName: billing.familyName
+            firstName: billingInfo.givenName,
+            lastName: billingInfo.familyName
         },
         hpmCreditCardPaymentMethodId: paymentMethodId,
         subscription: {
             termType: "EVERGREEN",
-            contractEffectiveDate: "TODO",
+            contractEffectiveDate: moment().format("YYYY-MM-DD"),
             subscribeToRatePlans: [
                 {
-                    productRatePlanId: planId
+                    productRatePlanId: subscriptionPlanId
                 }
             ]
         }
@@ -36,10 +35,24 @@ exports.handler = function(event, context) {
             "Content-Type": "application/json"
         }
     })
-    .then((response) => {
-        console.log(response);
+    .then(({data}) => {
+        if (data.success) {
+            console.log("ZUORA OK");
+        } else {
+            console.log("ZUORA ERROR");
+            console.log(data);
+            context.fail(JSON.stringify({
+                code: "ZuoraError",
+                ...data
+            }));
+        }
     })
     .catch((error) => {
+        console.log("ZUORA ERROR");
         console.log(error);
+        context.fail(JSON.stringify({
+            code: "ZuoraError",
+            ...error
+        }));
     });
 };
